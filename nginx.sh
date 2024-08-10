031c622dcab0c37a330cddbb255b81793d3c3787
YelehaUwU <162886331+YelehaUwU@users.noreply.github.com>
2024-08-10 20:34:54 +0200

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rm -rf /home/container/tmp/*
-PHP_VERSION=$(cat "/home/container/php_version.txt")
 
 echo "[Docker] Starting PHP-FPM"
 php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf --daemonize > /dev/null 2>&1
